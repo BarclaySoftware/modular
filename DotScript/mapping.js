96d3b00bf9d388ec01eec1b0c302fb2bfe697d4f
thejupitergroup <ourjupitergroup@gmail.com>
2024-08-23 23:16:10 -0400

    Beta 1.8

--- DotScript/mapping.js
+++
@@ -1,9 +0,0 @@
-export const customToJSMapping = {
-    'call': 'function',
-    'meet': 'name',
-    'output': 'console.log',
-    'when': 'if',
-    'higher': '>',
-    'or_else': 'else',
-    'digit': 'number',
-};
